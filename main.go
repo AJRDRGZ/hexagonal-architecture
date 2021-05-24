63e620f153acae0a9770c5d7e54f01c23cd650a5
Alejandro Rodríguez <aj.softwaredeveloper@gmai.com>
2021-05-24 13:58:08 -0500

    Initialization in 3 steps

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/AJRDRGZ/hexagonal-architecture/domain/poetry"
+	"github.com/AJRDRGZ/hexagonal-architecture/infrastructure/console"
+	"github.com/AJRDRGZ/hexagonal-architecture/infrastructure/postgres"
+)
 
+func main() {
+	// 1. Instantiate right-side adapters (I want to go out)
+	poetryLibrary := postgres.New()
+
+	// 2. Instantiate the hexagon
+	poetryReader := poetry.NewReader(poetryLibrary)
+
+	// 3. Instantiate the left-side adapter(s)
+	consoleAdapter := console.New(poetryReader)
+
+	consoleAdapter.Ask()
+
+	// NOTE:
+	// El adaptador de Izquierda (consoleAdapter) MANEJA la lógica de negocio (poetryReader),
+	// y el adaptador de derecha (poetryLibrary) es MANEJADO por la lógica de negocio.
 }
